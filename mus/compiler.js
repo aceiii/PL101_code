f1178c4a7d9430dc48f2c7b224646c1a2fa5fb61
Borin Ouch <borin.ouch@gmail.com>
2015-08-29 05:52:00 -0700

    Add rest & par tags, midi pitch numbers

--- mus/compiler.js
+++ mus/compiler.js
@@ -1,19 +1,34 @@
 
 
-
-// maybe some helper functions
-
 var absexpr = function(time, expr) {
     return {
         tag: 'note',
-        pitch: expr.pitch,
+        pitch: convertPitch(expr.pitch),
         dur: expr.dur,
         start: time
     };
 };
 
+var noteIndex = function(note) {
+    switch(note) {
+        case "c": return 0;
+        case "d": return 2;
+        case "e": return 4;
+        case "f": return 5;
+        case "g": return 7;
+        case "a": return 9;
+        case "b": return 11;
+    }
+    return -1;
+}
+var convertPitch = function(name) {
+    var s = name.toLowerCase().split("");
+    var note = noteIndex(s[0]);
+    var num = parseInt(s[1]);
+    return 12 + (note + (12 * num));
+};
+
 var compile = function (musexpr, t) {
-    // your code here
 
     var time = t;
     if (time === null || time === undefined) {
@@ -24,33 +39,43 @@ var compile = function (musexpr, t) {
         return [absexpr(time, musexpr)];
     } else if (musexpr.tag === 'seq') {
         var left = compile(musexpr.left, time);
-        var last = left[left.length - 1];
-        var right = compile(musexpr.right, last.start + last.dur);
+        var right = compile(musexpr.right, endTime(time, musexpr.left));
         return left.concat(right);
+    } else if (musexpr.tag === 'par') {
+        left = compile(musexpr.left, time);
+        right = compile(musexpr.right, time);
+        return left.concat(right);
+    } else {
+        return [];
     }
 };
 
 var endTime = function (time, expr) {
-    // your code here
+
+    if (expr === null || expr === undefined) {
+        return 0;
+    }
 
     if (expr.tag === 'note') {
         return time + expr.dur;
     } else if (expr.tag === 'seq') {
         return time + endTime(0, expr.left) + endTime(0, expr.right);
+    } else if (expr.tag === 'par') {
+        return time + Math.max(endTime(0, expr.left), endTime(0, expr.right));
+    } else if (expr.tag === 'rest') {
+        return time + expr.duration;
     }
 };
 
 var reverse = function(expr) {
-    // Your code here
-
-    if (expr.tag === 'note') {
-        return expr;
-    } else if (expr.tag === 'seq') {
+    if (expr.tag === 'seq' || expr.tag === 'par') {
         return {
             tag: 'seq',
             left: reverse(expr.right),
             right: reverse(expr.left)
         };
+    } else {
+        return expr;
     }
 };
 
@@ -69,8 +94,42 @@ var melody_mus = {
     }
 };
 
+var melody_mus2 = {
+    tag: 'seq',
+    left: {
+        tag: 'seq',
+        left: { tag: 'note', pitch: 'a4', dur: 250 },
+        right: { tag: 'rest', duration: 120 },
+    },
+    right: {
+        tag: 'seq',
+        left: { tag: 'note', pitch: 'c4', dur: 500 },
+        right: { tag: 'note', pitch: 'd4', dur: 500 }
+    }
+};
+
+var melody_mus3 = {
+    tag: 'seq',
+    left: {
+        tag: 'par',
+        left: { tag: 'note', pitch: 'a4', dur: 250 },
+        right: { tag: 'rest', duration: 120 },
+    },
+    right: {
+        tag: 'seq',
+        left: { tag: 'note', pitch: 'c4', dur: 500 },
+        right: { tag: 'note', pitch: 'd4', dur: 500 }
+    }
+}
+
 console.log(melody_mus);
 console.log(compile(melody_mus));
+console.log("\n")
 
+console.log(melody_mus2);
+console.log(compile(melody_mus2));
+console.log("\n");
 
-
+console.log(melody_mus3);
+console.log(compile(melody_mus3));
+console.log("\n");
