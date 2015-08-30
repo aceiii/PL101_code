ba55b59ebe28e69a7ece1ce76cd8fe756f7538b1
Borin Ouch <borin.ouch@gmail.com>
2015-08-30 00:05:19 -0700

    Support for mix of different types of whitespace, add testing

--- scheem/main.js
+++ scheem/main.js
@@ -2,12 +2,21 @@
 var PEG = require("pegjs");
 var assert = require("assert");
 var fs = require("fs");
+var path = require("path");
 
-var data = fs.readFileSync("scheem.peg", "utf-8");
-
+var data = fs.readFileSync(path.join(__dirname, "scheem.peg"), "utf-8");
 //console.log(data);
 
 var parse = PEG.buildParser(data).parse;
 
+var chai = require("chai");
+var assert = chai.assert;
+
 assert.deepEqual(parse("(a b c)"), ["a", "b", "c"]);
 
+// test whitespaces: space, multiple spaces, tabs, newlines
+assert.deepEqual(parse("( a   b\t \tc)"), ["a", "b", "c"]);
+assert.deepEqual(parse("(a\nb\nc)"), ["a", "b", "c"]);
+assert.deepEqual(parse("(a\n\t b\n c)"), ["a", "b", "c"]);
+
+assert.deepEqual(parse("(a \n(b c)\n d )"), ["a", ["b", "c"], "d"]);
